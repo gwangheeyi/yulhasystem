4b63f53e779ce2748f370b68ad8f0e0a787abd21
gwangheeyi <gwanghee@gmail.com>
2024-03-18 15:06:19 +0900

    탭설정

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
       vsync: this,
       initialIndex: 0,
       //탭 변경 애니메이션 시간
-      animationDuration: const Duration(microseconds: 800),
+      animationDuration: const Duration(microseconds: 100),
   );
 
   @override
@@ -35,32 +35,30 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
     return Scaffold(
       appBar: AppBar(
         title: const Text("율하스크린 주문 시스템"),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: const [
+            Tab(icon:Icon(Icons.coffee),text:"음료주문"),
+            Tab(icon:Icon(Icons.sports_golf_sharp),text:"게임문의"),
+            Tab(icon:Icon(Icons.golf_course),text: "홀인원 기록"),
+            Tab(icon:Icon(Icons.notifications),text:"공지사항")
+          ],
+          labelColor: Colors.white,
+          labelStyle: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w900,
+            //backgroundColor: Colors.blue,
+          ),
+          indicatorWeight: 5,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicatorColor: Colors.blue,
+          indicator: BoxDecoration(
+              color: Colors.red.shade900,
+              borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+        ),
       ),
-      body:_tabBar(),
-    );
-  }
-
-  Widget _tabBar(){
-    return TabBar(
-      controller: tabController,
-      tabs: const [
-        Tab(icon:Icon(Icons.coffee),text:"음료주문"),
-        Tab(icon:Icon(Icons.sports_golf_sharp),text:"게임문의"),
-        Tab(icon:Icon(Icons.golf_course),text: "홀인원 기록"),
-        Tab(icon:Icon(Icons.notifications),text:"공지사항")
-      ],
-      labelColor: Colors.white,
-      labelStyle: const TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.w900,
-      ),
-      indicatorWeight: 5,
-      indicatorSize: TabBarIndicatorSize.tab,
-      indicatorColor: Colors.blue,
-      indicator: BoxDecoration(
-        color: Colors.red.shade900,
-        borderRadius: BorderRadius.all(Radius.circular(10))
-      ),
+      body:null,
     );
   }
 }
