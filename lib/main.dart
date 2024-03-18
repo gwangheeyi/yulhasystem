12b1c747c03731def2dc4168777beae553f35dfd
gwangheeyi <gwanghee@gmail.com>
2024-03-18 13:16:34 +0900

    탭설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  runApp(
+      MaterialApp(home:const TabScreen()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TabScreen> createState() => _TabScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMixin{
 
-  void _incrementCounter() {
-    setState(() {
+  late TabController tabController = TabController(
+      length: 4,
+      vsync: this,
+      initialIndex: 0,
+      //탭 변경 애니메이션 시간
+      animationDuration: const Duration(microseconds: 800),
+  );
 
-      _counter++;
-    });
+  @override
+  void dispose(){
+    tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+        title: const Text("율하스크린 주문 시스템"),
       ),
-      body: Center(
+      body:_tabBar(),
+    );
+  }
 
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '버튼을 누르시오:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+  Widget _tabBar(){
+    return TabBar(
+      controller: tabController,
+      tabs: const [
+        Tab(icon:Icon(Icons.coffee),text:"음료주문"),
+        Tab(icon:Icon(Icons.sports_golf_sharp),text:"게임문의"),
+        Tab(icon:Icon(Icons.golf_course),text: "홀인원 기록"),
+        Tab(icon:Icon(Icons.notifications),text:"공지사항")
+      ],
+      labelColor: Colors.white,
+      labelStyle: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.w900,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      indicatorWeight: 5,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorColor: Colors.blue,
+      indicator: BoxDecoration(
+        color: Colors.red.shade900,
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
     );
   }
 }
